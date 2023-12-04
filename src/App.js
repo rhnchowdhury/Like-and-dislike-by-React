9dfc63d1079eed8ef175f0f496642e60f4ba344f
rhnchowdhury <rhnchdri@gmail.com>
2023-12-04 17:02:33 +0600

    project add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import img from '../src/images/jawan.jfif';
+import { AiFillLike } from "react-icons/ai";
+import { useState } from 'react';
 
 function App() {
+  const [like, setLike] = useState(0);
+  const [isLike, setIsLike] = useState(false);
+
+  const likeClick = () => {
+    setLike(like + (isLike ? -1 : 1));
+    setIsLike(!isLike);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Like-Dislike State</h1>
+      <div>
+        <h5 className='like-5'>Like button</h5>
+        <span className={(isLike ? "like-color" : 'dislike-color')}>
+          <AiFillLike className='like-icon' onClick={likeClick} />
+        </span>
+        <h6>Likes: {like}</h6>
+      </div>
+      {/* <div>
+        <h4>Like Count:</h4>
+        <img src={img} alt="" />
+      </div> */}
     </div>
   );
 }
