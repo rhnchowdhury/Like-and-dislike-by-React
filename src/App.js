103a3c14c07c8d85c232af9988e3b12265a24268
rhnchowdhury <rhnchdri@gmail.com>
2023-12-05 18:37:07 +0600

    done all

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import img from '../src/images/jawan.jfif';
 import { AiFillLike } from "react-icons/ai";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
 import { useState } from 'react';
 
 function App() {
   const [like, setLike] = useState(0);
   const [isLike, setIsLike] = useState(false);
+  const [love, setLove] = useState(false);
+  const [count, setCount] = useState(0);
 
   const likeClick = () => {
     setLike(like + (isLike ? -1 : 1));
     setIsLike(!isLike);
-  }
+  };
+
+  const handleLikes = () => {
+    if (!love) {
+      setLove(true);
+      setCount(count + 1);
+    } else {
+      setLove(false);
+      setCount(count - 1);
+    }
+  };
 
   return (
     <div className="App">
@@ -22,10 +36,16 @@ function App() {
         </span>
         <h6>Likes: {like}</h6>
       </div>
-      {/* <div>
-        <h4>Like Count:</h4>
-        <img src={img} alt="" />
-      </div> */}
+      <div>
+        <h4>Like Count: {count}</h4>
+        <div>
+          <img src={img} alt="" />
+          {
+            love ? <p className='icon-p'><FaHeart className='icon' onClick={handleLikes} /></p>
+              : <p className='icon-p'><CiHeart className='icon' onClick={handleLikes} /></p>
+          }
+        </div>
+      </div>
     </div>
   );
 }
